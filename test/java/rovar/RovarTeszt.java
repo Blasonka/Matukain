78f6d63abab682b4c0d4035f5dbba2fcf5e82bce
Jónás Gergely Péter <jonas.gergely24@gmail.com>
2025-03-23 13:27:35 +0100

    JUnit library hozzáadva

---
+++ test/java/rovar/RovarTeszt.java
@@ -0,0 +1,10 @@
+package java.rovar;
+
+import org.junit.Assert;
+
+public class RovarTeszt {
+    public static void main(String[] args) {
+        System.out.println("RovarTeszt");
+        Assert.assertEquals(1, 1);
+    }
+}
