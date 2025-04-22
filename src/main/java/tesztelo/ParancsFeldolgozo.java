8a9d1ad24c1770064792cd353aa3c83195779b54
todortoth <todortoth@gmail.com>
2025-04-22 15:06:04 +0200

    adds parancs

--- src/main/java/tesztelo/ParancsFeldolgozo.java
+++ src/main/java/tesztelo/ParancsFeldolgozo.java
@@ -3,6 +3,7 @@ package tesztelo;
 import felhasznalo.Gombasz;
 import felhasznalo.Rovarasz;
 import gomba.Gomba;
+import gomba.Gombafonal;
 import jateklogika.gameLogic;
 import rovar.Rovar;
 import tekton.*;
@@ -92,6 +93,9 @@ public class ParancsFeldolgozo {
                 case "/addt":
                     addt(Integer.parseInt(command[1]), Integer.parseInt(command[2]), Integer.parseInt(command[3]), command[4].charAt(0));
                     break;
+                case "/adds":
+                    adds(Integer.parseInt(command[1]), Integer.parseInt(command[2]), Integer.parseInt(command[3]), Integer.parseInt(command[4]), command[5].charAt(0));
+                    break;
             }
         } catch (NumberFormatException e) {
             System.out.println("A parancs valamely paraméterének formátuma nem megfelelő!");
@@ -160,9 +164,18 @@ public class ParancsFeldolgozo {
         }
     }
 
+    /**
+     * Tekton hozzáadása
+     * @param id tekton id-ja
+     * @param x tekton x koordinátája
+     * @param y tekton y koordinátája
+     * @param type tekton típusa [L (életbentartó), M (egyfonal) F (felszívódós), T (többfonalas) G (gombatest nélküli)]
+     */
     private void addt(int id, int x, int y, char type) {
+        // TODO Tekton szomszédosság lekezelése
         switch (type) {
             case 'L':
+                // TODO Életbentrartó tekton implementálása
                 break;
             case 'M':
                 jatekLogika.getMapTekton().add(new MaxEgyFonalTekton(id, x, y));
@@ -175,6 +188,32 @@ public class ParancsFeldolgozo {
                 break;
             case 'G':
                 jatekLogika.getMapTekton().add(new GombatestNelkuliTekton(id, x, y));
+                break;
+        }
+    }
+
+    /**
+     * Spóra felvétele
+     * @param id azonosító
+     * @param tektonID tekton, amelyen a spóra megtalálható
+     * @param gombaID gomba, amelyhez a spóra tartozik
+     * @param jatekosID játékos, akihez a spóra tartozik
+     * @param type spóra típusa [O (osztó), L (lassító), G (gyorsító), B (bénító), V (vágásgátló)]
+     */
+    private void adds (int id, int tektonID, int gombaID, int jatekosID, char type) {
+        // TODO spórák tárolásának implementálása
+        Gomba g = ((Gombasz) jatekLogika.jatekosok.get(jatekosID)).getGombak().get(gombaID);
+        switch(type) {
+            case 'O':
+                // TODO RovarOsztóSpóra implementálása
+                break;
+            case 'L':
+                break;
+            case 'G':
+                break;
+            case 'B':
+                break;
+            case 'V':
                 break;
         }
     }
